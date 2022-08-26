deb0f1013a3aa6a868b5d0e27d63981a9e23b623
justynlewis <justynlewis1@gmail.com>
2022-08-26 17:30:35 -0400

    Finish UI Version

--- script.js
+++ script.js
@@ -1,8 +1,70 @@
+const body = document.querySelector('body');
+
+const prompt = document.createElement('div');
+prompt.textContent = "Choose rock, paper, or scissors";
+body.appendChild(prompt);
+
+
+const rockButton = document.createElement('button');
+rockButton.classList.add('rock');
+rockButton.textContent = "Rock";
+body.appendChild(rockButton);
+
+const paperButton = document.createElement('button');
+paperButton.classList.add('paper');
+paperButton.textContent = "Paper";
+body.appendChild(paperButton);
+
+const scissorsButton = document.createElement('button');
+scissorsButton.classList.add('scissors');
+scissorsButton.textContent = "Scissors";
+body.appendChild(scissorsButton);
+
+
+let playerWins = 0;
+let computerWins = 0;
+
+const computerChose = document.createElement('div');
+body.appendChild(computerChose);
+
+const thisRound = document.createElement('div');
+body.appendChild(thisRound);
+
+const runningScore = document.createElement('div');
+runningScore.textContent = ("Your wins: " + playerWins + ", Computer Wins: " + computerWins);
+body.appendChild(runningScore);
+
+const gameOver = document.createElement('div');
+body.appendChild(gameOver);
+
+
+
+let roundResults = "";
+
+
+rockButton.addEventListener('click', function () {
+    roundResults = playRound("rock", getComputerChoice());
+    thisRound.textContent = roundResults;
+    scoreTallyer(roundResults);
+})  
+paperButton.addEventListener('click', function () {
+    roundResults = playRound("paper", getComputerChoice());
+    thisRound.textContent = roundResults;
+    scoreTallyer(roundResults);
+})
+scissorsButton.addEventListener('click', function () {
+    roundResults = playRound("scissors", getComputerChoice());
+    thisRound.textContent = roundResults; 
+    scoreTallyer(roundResults);
+})
+
+
+
 function getComputerChoice() {
     const choices = ["Rock", "Paper", "Scissors"];
     let randNumber = Math.floor(Math.random() * choices.length);
     let computerChoice = choices[randNumber];
-    console.log(computerChoice);
+    computerChose.textContent = ("Computer Chose: " + computerChoice);
     return computerChoice;
 }
 
@@ -31,34 +93,23 @@ function playRound(playerSelection, computerSelection) {
             case "SCISSORS": return "You Win! Scissor beats paper";
         }
     }
-
    }
 }
 
-
-function game() {
-    let playerWins = 0;
-    let computerWins = 0;
-    for (let i = 0; i < 5; i++) {
-        let playerChoice = prompt("Choose rock, paper, or scissors");
-        let roundResults = playRound(playerChoice, getComputerChoice());
-        console.log(roundResults);
-        if (roundResults.includes("Win")) {
-            playerWins++;
-        }
-        else if (roundResults.includes("Lose")) {
-            computerWins++;
-        }
+function scoreTallyer(roundResults) {
+    if (roundResults.includes("Win")) {
+        playerWins++;
     }
-    if (playerWins > computerWins) {
-        console.log("Game Over! You win!");
+    else if (roundResults.includes("Lose")) {
+        computerWins++;
     }
-    else if (computerWins > playerWins) {
-        console.log("Game Over! You lost.");
+    
+    runningScore.textContent = ("Your wins: " + playerWins + ", Computer Wins: " + computerWins);
+    
+    if (playerWins === 5) {
+        gameOver.textContent = "Game Over! You win!";
     }
-    else {
-        console.log("Game Over! Tie Game...");
+    else if (computerWins === 5) {
+        gameOver.textContent = "Game Over! You lost!";
     }
 }
-
-game();
